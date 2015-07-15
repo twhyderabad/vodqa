4e7d7cad42e18f106f77b720f96a492735231e94
Nandhakumar Ramanathan <nandhar@thoughtworks.com>
2015-07-15 16:45:26 +0530

    FORM | Fix select box arrow, Fix footer image, Remove styles on window resize

--- dist/assets/js/mkto-style-overrides.js
+++ dist/assets/js/mkto-style-overrides.js
@@ -1,5 +1,12 @@
-console.log("override styles ...")
 MktoForms2.onFormRender(function() {
+  console.log("override styles ...")
+  e = $(".register .mktoForm *")
+  e.removeAttr("style")
+  $("#mktoForm_3106").removeAttr("style")
+})
+
+$(window).resize(function() {
+  console.log("override styles ...")
   e = $(".register .mktoForm *")
   e.removeAttr("style")
   $("#mktoForm_3106").removeAttr("style")
