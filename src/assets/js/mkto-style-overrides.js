e5fc27b88dc0afd1b65914bfa13a10a0d0dce83f
Abdulsattar Mohammed <asattar.md@gmail.com>
2015-07-27 19:09:04 +0530

    Changed the form

--- src/assets/js/mkto-style-overrides.js
+++ src/assets/js/mkto-style-overrides.js
@@ -2,12 +2,12 @@ MktoForms2.onFormRender(function() {
   console.log("override styles ...")
   e = $(".register .mktoForm *")
   e.removeAttr("style")
-  $("#mktoForm_3106").removeAttr("style")
+  $("#mktoForm_3219").removeAttr("style")
 })
 
 $(window).resize(function() {
   console.log("override styles ...")
   e = $(".register .mktoForm *")
   e.removeAttr("style")
-  $("#mktoForm_3106").removeAttr("style")
+  $("#mktoForm_3219").removeAttr("style")
 })
