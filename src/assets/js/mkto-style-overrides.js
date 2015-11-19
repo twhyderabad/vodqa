9ad237d3d34bcac399df7b996bbac26714ca2b21
Abdulsattar Mohammed <asattar.md@gmail.com>
2015-11-19 16:58:50 +0530

    New Vodqa styles

--- src/assets/js/mkto-style-overrides.js
+++ src/assets/js/mkto-style-overrides.js
@@ -1,16 +1,16 @@
 MktoForms2.onFormRender(function() {
-  console.log("override styles ...")
-  e = $(".register .mktoForm *")
-  e.removeAttr("style")
-  $("#mktoForm_3219").removeAttr("style")
+  console.log("override styles ...");
+  e = $(".register .mktoForm *");
+  e.removeAttr("style");
+  $("#mktoForm_3823").removeAttr("style");
   $("#job_title__c").val('Tester');
   $('#Industry').val('Software & Internet');
   $('.mktoButton').html("Register").parent().after('<span class="tag">*No Charges Applicable</span>');
-})
+});
 
 $(window).resize(function() {
-  console.log("override styles ...")
-  e = $(".register .mktoForm *")
-  e.removeAttr("style")
-  $("#mktoForm_3219").removeAttr("style")
-})
+  console.log("override styles ...");
+  e = $(".register .mktoForm *");
+  e.removeAttr("style");
+  $("#mktoForm_3823").removeAttr("style")
+});
