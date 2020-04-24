da0a2e728480341bde89af87de5983b8d3938e71
biswajit <biswajip@BiswajitMacBook-Pro.local>
2020-04-24 16:07:47 +0530

    changes for vodqa shots May 2020

--- dist/assets/js/editions.js
+++ dist/assets/js/editions.js
@@ -1,23 +1,23 @@
 function openNav(editionURL) {
     const accessControl = "Access-Control-Allow-Origin";
-    $.ajax({
-        url: editionURL,
-        headers : {accessControl: "*"}       
-        }).done(function(data) {
-            $('.temp-div').html(data);
-        }).done(function() {
-            var d = document.getElementsByClassName('temp-div');
-            var f = d[0].querySelector("#agenda");
-            document.getElementsByClassName("overlay-content")[0].innerHTML = f.innerHTML;
-            document.getElementById("myNav").style.display = "block";
-        });
+    // $.ajax({
+    //     url: editionURL,
+    //     headers : {accessControl: "*"}       
+    //     }).done(function(data) {
+    //         $('.temp-div').html(data);
+    //     }).done(function() {
+    //         var d = document.getElementsByClassName('temp-div');
+    //         var f = d[0].querySelector("#agenda");
+    //         document.getElementsByClassName("overlay-content")[0].innerHTML = f.innerHTML;
+    //         document.getElementById("myNav").style.display = "block";
+    //     });
 
-    // $('.temp-div').load(editionURL, function() {
-    //     var d = document.getElementsByClassName('temp-div');
-    //     var f = d[0].querySelector("#agenda");
-    //     document.getElementsByClassName("overlay-content")[0].innerHTML = f.innerHTML;
-    //     document.getElementById("myNav").style.display = "block";
-    // });
+    $('.temp-div').load(editionURL, function() {
+        var d = document.getElementsByClassName('temp-div');
+        var f = d[0].querySelector("#agenda");
+        document.getElementsByClassName("overlay-content")[0].innerHTML = f.innerHTML;
+        document.getElementById("myNav").style.display = "block";
+    });
 }
 
 function closeNav() {
